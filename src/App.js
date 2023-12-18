3225a4063db14377fa73a713ef28c5ee997e2fe8
Sara Girard <sara@paditim.com>
2023-12-18 15:21:25 -0500

    update to currency and base on app

--- src/App.js
+++ src/App.js
@@ -2,44 +2,34 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Code to import Budget.js
 import Budget from './components/Budget';
 
-// Add code to import the other components here under
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import Currency from './components/Currency';
 
 import { AppProvider } from './context/AppContext';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
-
-                        {
-                            /* Add Remaining component here*/
-                        }        
-
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
-                </div>
+                        <div className='col-sm'>
+                            {/* <Budget /> */}
+                        </div>  
+                        <div className='col-sm'>
+                            {/* <Remaining /> */}
+                        </div>
+                        <div className='col-sm'>
+                            {/* <ExpenseTotal /> */}
+                        </div>
+                        <div className='col-sm'>
+                            <Currency />
+                        </div>
+                    </div>   
+                    
             </div>
         </AppProvider>
     );
